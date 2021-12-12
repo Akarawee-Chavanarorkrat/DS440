0936c0bf2e02d4bcad7ad9adc6a8247c9a4f1855
Akarawee-Chavanarorkrat <60050750+Akarawee-Chavanarorkrat@users.noreply.github.com>
2021-12-11 19:38:54 -0500

    Add files via upload

---
+++ Countdowntimer.js
@@ -0,0 +1,42 @@
+var h3 = document.getElementsByTagName("h3");
+
+var sec         = 10,
+    countDiv    = document.getElementById("timer"),
+    secpass,
+    countDown   = setInterval(function () {
+        'use strict';
+        
+        secpass();
+    }, 1000);
+
+function secpass() {
+    'use strict';
+    var hour	= Math.floor(sec/3600)
+    var min     = Math.floor(sec / 60),
+        remSec  = sec % 60;
+    
+    if (remSec < 10) {
+        
+        remSec = '0' + remSec;
+    
+    }
+    if (min < 10) {
+        
+        min = '0' + min;
+    
+    }
+    countDiv.innerHTML = hour + ":" + min + ":" + remSec;
+    
+    if (sec > 0) {
+        
+        sec = sec - 1;
+        
+    } else {
+        
+        clearInterval(countDown);
+        
+        countDiv.innerHTML = 'countdown done';
+		alert("Countdown Timer over Feel free to take a short break.");
+        
+    }
+}// JavaScript Document
